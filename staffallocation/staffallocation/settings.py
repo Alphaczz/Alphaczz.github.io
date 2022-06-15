771bbb37cab2f4b42db5ff4c8f13f3337765b7d4
Shivam Rajesh Pandey <87713804+Alphaczz@users.noreply.github.com>
2022-06-15 13:51:04 +0530

    ol

--- staffallocation/staffallocation/settings.py
+++ staffallocation/staffallocation/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE='whitenoise.storage.CompressManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR /'staticfiles'
+STATIC_ROOT = BASE_DIR /'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
